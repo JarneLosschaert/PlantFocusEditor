d835182345a214f3de4af27dbc8814817a9da6cb
Ludwig Delanghe <ludwig3.delanghe@gmail.com>
2024-03-18 14:47:28 +0100

    fixed bug

--- PlantFocusEditor/wwwroot/js/renderPreview.js
+++ PlantFocusEditor/wwwroot/js/renderPreview.js
@@ -11,7 +11,7 @@ function renderPreviewFromJson(jsonToRender) {
     }
     let width;
     let height;
-    
+
     const layer = new Konva.Layer();
     const group = Konva.Node.create(json.Group);
 
@@ -42,35 +42,6 @@ function renderPreviewFromJson(jsonToRender) {
     const dataUrl = stage.toDataURL({ pixelRatio: 2 });
     container.remove();
     return dataUrl;
-}
-
-function convertJsonToKonva(stage, json) {
-    const layer = new Konva.Layer();
-    const group = Konva.Node.create(json.Group);
-
-    group.children.forEach((child, i, arr) => {
-        const node = Konva.Node.create(child);
-        arr[i] = node;
-        if (node.getClassName() === "Path") {
-            width = findWidthPassePartout(node.data());
-            height = findHeightPassePartout(node.data());
-        } else if (node.getClassName() === "Image") {
-            const src = node.attrs.src;
-            const img = new Image();
-            img.src = src;
-            node.image(img);
-        } else if (node.getClassName() === "Shape") {
-            node.sceneFunc(sceneFunc);
-        }
-    });
-
-    layer.add(group);
-    const stage = new Konva.Stage({
-        container: container,
-        width: width, //(template.widthMillimeters * 72) / 25.4,
-        height: height //(template.heightMillimeters * 72) / 25.4
-    });
-    stage.add(layer);
 }
 
 function isValidJson(json) {
