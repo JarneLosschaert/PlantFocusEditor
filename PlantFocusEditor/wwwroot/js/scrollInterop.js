0e5010400bf330fa7f09c3dfcbc6f1037363e7f8
Jarne Losschaert <jelt@dms.be>
2024-03-20 13:51:54 +0100

    elements bar done

--- PlantFocusEditor/wwwroot/js/scrollInterop.js
+++
@@ -1,9 +0,0 @@
-window.scrollFunction = function (scrollAmount, className) {
-    var content = document.querySelector(`.${className}`);
-    var currentScrollAmount = parseFloat(content.style.getPropertyValue('--scroll-amount'));
-    content.style.setProperty('--scroll-amount', `${currentScrollAmount + scrollAmount}px`);
-    content.style.animation = 'scroll 0.5s linear';
-    setTimeout(() => {
-        content.style.animation = 'none';
-    }, 500);
-}
