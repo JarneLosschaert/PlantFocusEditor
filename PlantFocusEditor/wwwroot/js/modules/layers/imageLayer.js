50444dca4acb9444ff39d2dd5f6bbfd77e97e7e7
Jarne Losschaert <jelt@dms.be>
2024-03-27 13:50:41 +0100

    images styling + functionality fixed

--- PlantFocusEditor/wwwroot/js/modules/layers/imageLayer.js
+++ PlantFocusEditor/wwwroot/js/modules/layers/imageLayer.js
@@ -1,31 +1,34 @@
 import { addHoverAnimation } from "../animations.js";
 import { currentGroup } from "../constants.js";
 
-function addImage(id, src) {
+function addImage(src) {
     const img = new Image();
     img.src = src;
 
-    const scale = 200 / img.height;
+    img.onload = function () {
 
-    const kimg = new Konva.Image({
-        name: "image",
-        image: img,
-        src: img.src,
-        id: id,
-        height: img.height,
-        width: img.width,
-        scaleX: scale,
-        scaleY: scale,
-        draggable: true,
-        stroke: "#000000",
-        strokeWidth: 0,
-        shadowBlur: 10,
-        shadowOffset: { x: 5, y: 5 },
-        shadowOpacity: 0,
-        locked: false
-    });
-    currentGroup.add(kimg);
-    addHoverAnimation(kimg);
+        const width = 200;
+        const scale = width / img.width;
+
+        const kimg = new Konva.Image({
+            name: "image",
+            image: img,
+            src: img.src,
+            height: img.height,
+            width: img.width,
+            scaleX: scale,
+            scaleY: scale,
+            draggable: true,
+            stroke: "#000000",
+            strokeWidth: 0,
+            shadowBlur: 10,
+            shadowOffset: { x: 5, y: 5 },
+            shadowOpacity: 0,
+            locked: false
+        });
+        currentGroup.add(kimg);
+        addHoverAnimation(kimg);
+    };
 }
 
 export { addImage };
