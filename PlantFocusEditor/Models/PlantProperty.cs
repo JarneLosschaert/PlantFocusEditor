4754f65ca3b3fb2e15f35df7b25ced2fe03472fd
Jarne Losschaert <jelt@dms.be>
2024-04-19 14:46:11 +0200

    properties done

---
+++ PlantFocusEditor/Models/PlantProperty.cs
@@ -0,0 +1,9 @@
+﻿namespace PlantFocusEditor.Models
+{
+    public class PlantProperty
+    {
+        public string Name { get; set; } = "";
+        public string Icon { get; set; } = "https://plantfocus.blob.core.windows.net/icons/toepassing.png";
+        public List<Translation> Translations { get; set; } = new List<Translation>();
+    }
+}
